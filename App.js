f36e96d15f97615d68b4acca5aa6929f0b2e662e
Camacho <rafaelcamachofitti15@gmail.com>
2023-11-04 02:57:53 -0300

    ref/divisão em componentes Topo e Detalhes

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { SafeAreaView, StatusBar, View } from 'react-native';
-import Cesta from './src/screens/cesta'
 import { 
   useFonts, 
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat';
+import Cesta from './src/screens/Cesta'
+
 
 export default function App() {
 
--- src/screens/Cesta/componentes/Detalhes.js
+++ src/screens/Cesta/componentes/Detalhes.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Image, Dimensions, StyleSheet, Text, View } from 'react-native'
-import topo from '../../assets/topo.png';
+import {StyleSheet, View, Image} from 'react-native'
+import Texto from '../../../componentes/Texto'
 
-import Texto from '../componentes/Texto';
-
-const widht = Dimensions.get('screen').width;
-
-export default function Cesta() {
+export default function Detalhes() {
     return <>
-        <Image source={topo} style={estilos.topo} />
-        <Texto style={estilos.titulo}>Detalhe da cesta</Texto>
-
-        <View style={estilos.cesta}>
             <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
-                <Image source={require('../../assets/logo.png')} style={estilos.imagemFazenda} />
+                <Image source={require('../../../../assets/logo.png')} style={estilos.imagemFazenda} />
                 <Texto style={estilos.nomeFazenda}>Jenny Jack</Texto>
             </View>
             <Texto style={estilos.descricao}>
@@ -23,29 +15,10 @@ export default function Cesta() {
                 sua cozinha
             </Texto>
             <Texto style={estilos.preco}>R$40,00</Texto>
-        </View>
     </>
 }
 
 const estilos = StyleSheet.create({
-    topo: {
-        width: '100%',
-        height: 578/768 * widht,
-    },
-    titulo:{
-        width: '100%',
-        position: 'absolute',
-        textAlign: 'center',
-        fontSize: 16,
-        lineHeight: 26,
-        color: "white",
-        fontWeight: 'bold',
-        padding: 16,
-    },
-    cesta: {
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-    },
     nome: {
         fontSize: 26,
         lineHeight: 42,
@@ -78,4 +51,4 @@ const estilos = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 8,
     },
-});
+})
