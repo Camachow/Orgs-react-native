e537c6d34588d761a0e674f6b0c470eedb398271
Camacho <rafaelcamachofitti15@gmail.com>
2023-11-10 11:49:55 -0300

    ref/mudei map da lista de frutas por uma flatlist

--- src/screens/Cesta/componentes/Item.js
+++ src/screens/Cesta/componentes/Item.js
@@ -2,32 +2,20 @@ import React from 'react'
 import { Image, View, StyleSheet } from 'react-native'
 import Texto from '../../../componentes/Texto'
 
-export default function Itens({titulo, lista}) {
-    return <>
-        <Texto style={estilos.titulo}>{ titulo }</Texto>
-        {lista.map(({nome, imagem}) => {
-            return <View style={estilos.item}>
+export default function Item({ item: {nome, imagem }}) {
+    return  <View style={estilos.item}>
                 <Image source={imagem} style={estilos.imagem}/>
                 <Texto style={estilos.nome}>{nome}</Texto>
             </View>
-        })}
-    </>
 }
 
 const estilos = StyleSheet.create({
-    titulo: {
-        color:"#464646",
-        fontWeight: "bold",
-        marginTop: 32,
-        marginBottom: 8,
-        fontSize: 20,
-        lineHeight: 32,
-    },
     item: {
         flexDirection: "row",
         borderBottomWidth: 1,
         borderBottomColor: "#ECECEC",
         paddingVertical: 16,
+        marginHorizontal: 16,
         alignItems: "center",
     },
     imagem: {
